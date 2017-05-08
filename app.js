fbbbc372b4b8ebea6ffb6391873d9d5bdb96bb4a
Anton Kroisant <anton.kroisant@student.reutlingen-university.de>
2017-05-08 19:42:42 +0200

    init

---
+++ app.js
@@ -0,0 +1,16 @@
+var pc = new webkitRTCPeerConnection(servers,
+  {optional: [{RtpDataChannels: true}]});
+
+pc.ondatachannel = function(event) {
+  receiveChannel = event.channel;
+  receiveChannel.onmessage = function(event){
+    document.querySelector("div#receive").innerHTML = event.data;
+  };
+};
+
+sendChannel = pc.createDataChannel("sendDataChannel", {reliable: false});
+
+document.querySelector("button#send").onclick = function (){
+  var data = document.querySelector("textarea#send").value;
+  sendChannel.send(data);
+};
