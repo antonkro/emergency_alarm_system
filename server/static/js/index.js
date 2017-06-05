cdcbb9b606fdc07092d7e3f790948d3c7db70dd0
Anton Kroisant <anton.kroisant@student.reutlingen-university.de>
2017-06-05 15:52:55 +0200

    index site improved

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -1,58 +1,46 @@
 
-// var $ = require('jquery');
-
-// $(document).ready(function () {
-
 
 
 function init() {
-//     var incoming = document.getElementById("incoming");
-//     var privateButton = document.getElementById('private');
-//     var upgradeMsg = document.getElementById('upgrade-msg');
-//
-//     var socket = io()
-//     var opts = {peerOpts: {trickle: false}, autoUpgrade: false}
-//     var p2p = new P2P(socket, opts, function () {
-//         privateButton.disabled = false
-//         p2p.emit('peer-obj', 'Hello there. I am ' + p2p.peerId)
-//     })
-//
-//
-//     privateButton.addEventListener('click', function (e) {
-//         goPrivate()
-//         p2p.emit('go-private', true)
-//     })
-//
-//     p2p.on('go-private', function () {
-//         goPrivate()
-//     })
-//
-//     function goPrivate() {
-//         p2p.useSockets = false
-//         upgradeMsg.innerHTML = 'WebRTC connection established!'
-//         privateButton.disabled = true
-//     }
-//
-//     console.log("HEERE");
-//
-//     // socket = io.connect("http://localhost:3000");
-//     // socket.on('connect', function () {
-//
-//     // socket.on('data', function (data) {
-//     //     console.log(data);
-//     //     $("#incoming").text(data);
-//     // });
-//
-//     /*p2p.on('ready', function () {
-//      p2p.usePeerConnection = true;
-//      p2p.emit('peer-obj', {peerId: peerId});
-//      })*/
-//
-// // this event will be triggered over the socket transport
-// // until `usePeerConnection` is set to `true`
-//     p2p.on('data', function (data) {
-//         console.log(data);
-//         $("#incoming").text(data);
-//     });
+    var person = document.getElementById("person");
+    var contact = document.getElementById("contact");
+
+    var person_nav = document.getElementById("person_nav");
+    var contact_nav = document.getElementById("contact_nav");
+
+    //Greeting line
+    // setTimeout(disableGreeter(), 5000);
+
+    person.style.display = 'none';
+    contact.style.display = 'none';
+
+    document.getElementById("person_nav").addEventListener("click", function(){
+        person.style.display = 'block';
+        contact.style.display = 'none';
+        document.getElementById('person_nav').className ='active';
+        document.getElementById('contact_nav').className ='';
+
+    });
+
+    document.getElementById("contact_nav").addEventListener("click", function(){
+        person.style.display = 'none';
+        contact.style.display = 'block';
+        document.getElementById('person_nav').className ='';
+        document.getElementById('contact_nav').className ='active';
+    });
+
+    document.getElementById("home_nav").addEventListener("click", function(){
+        person.style.display = 'none';
+        contact.style.display = 'none';
+        document.getElementById('person_nav').className ='';
+        document.getElementById('contact_nav').className ='';
+    });
+
+
+
 }
 document.addEventListener('DOMContentLoaded', init, false);
+
+// function disableGreeter(){
+//     document.getElementById("welcome").style.display='none';
+// }
