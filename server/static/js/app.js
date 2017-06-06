25ad80e0158d4665afa19e5567d24b5693b0f3b0
DunkinDonat <donat.spam@t-online.de>
2017-06-06 15:29:16 +0200

    hello

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -14,11 +14,19 @@ function loginFailure(errorCode, message) {
     easyrtc.showError(errorCode, message);
 }
 
+function mergAllData(personenDaten, notfallkontakt, location){
+   var alldata = "{'person':"+personenDaten + ", 'contact': " + notfallkontakt + ", 'location: '"+location+ "}";
+   
+    return alldata;
+}
+
 function sendData(roomName, occupants, isPrimary){
-    var personenDaten = android.getPersonenDaten();
-    var kontaktdaten = android.getNotfallKontakt();
+    //var personenDaten = android.getPersonenDaten();
+    //var kontaktdaten = android.getNotfallKontakt();
+
     var location = "{lat:'48.77930', lng:'9.10717'}";
+    var data = mergAllData(android.getPersonenDaten(), android.getNotfallKontakt(), location)
     for (var easyrtcid in occupants){
-        easyrtc.sendDataWS(easyrtcid, "message", personenDaten,kontaktdaten,location);
+        easyrtc.sendDataWS(easyrtcid, "message", data );
     }
 }
