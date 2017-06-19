9ea9336674d16a287c64924b3fb9f96194e69488
DunkinDonat <donat.spam@t-online.de>
2017-06-19 13:31:47 +0200

    hello

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -25,7 +25,7 @@ function sendData(roomName, occupants, isPrimary){
     //var kontaktdaten = android.getNotfallKontakt();
 
     //var location = '{"lat":"48.77930", "lng":"9.10717"}';
-    var loaction = android.getPostition();
+    var loaction = android.getPosition();
     var data = mergeAllData(android.getPersonenDaten(), android.getNotfallKontakt(), location)
     for (var easyrtcid in occupants){
         easyrtc.sendDataWS(easyrtcid, "message", data );
