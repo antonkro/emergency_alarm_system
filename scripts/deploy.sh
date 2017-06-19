1ae810301c958394e44f0c52ded9bcbdb024a470
root <root@serv.kroisant.de>
2017-06-19 11:44:52 +0100

    deploy ports modifed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 cd /var/projects/emergency_alarm_system/server
 docker build -t antonkro/emergency_alarm_server .
-docker run -p 49160:443 --name=emergency_alarm_system -d antonkro/emergency_alarm_server 
+docker run -p 8080:443 --name=emergency_alarm_system -d antonkro/emergency_alarm_server 
 
--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -4,5 +4,5 @@ cd /var/projects/emergency_alarm_system/server
 docker build -t antonkro/emergency_alarm_server .
 docker stop  emergency_alarm_system 
 docker rm emergency_alarm_system 
-docker run -p 49160:443 --name=emergency_alarm_system -d antonkro/emergency_alarm_server
+docker run -p 8080:443 --name=emergency_alarm_system -d antonkro/emergency_alarm_server
 
