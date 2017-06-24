ed0565cadea4536b89ef24096fb8d69fe6e04328
Anton Kroisant <anton.kroisant@student.reutlingen-university.de>
2017-06-24 16:25:24 +0200

    button

--- server/static/js/app.js
+++ server/static/js/app.js
@@ -1,5 +1,5 @@
 function connect() {
-    easyrtc.setPeerListener();
+    easyrtc.setPeerListenser();
     easyrtc.setRoomOccupantListener(sendData);
     easyrtc.connect("easyrtc.instantMessaging", loginSuccess, loginFailure);
 }
@@ -7,7 +7,6 @@ function connect() {
 function loginSuccess(easyrtcid) {
     selfEasyrtcid = easyrtcid;
     console.log("I am " + easyrtcid);
-    se
 }
 
 
@@ -15,27 +14,20 @@ function loginFailure(errorCode, message) {
     easyrtc.showError(errorCode, message);
 }
 
-function mergeAllData(personenDaten, notfallkontakt, location) {
-    var alldata = '{"person" : ' + personenDaten + ', "contact" : ' + notfallkontakt + ', "location" : ' + location + '}';
+function mergeAllData(personenDaten, notfallkontakt, location){
+   var alldata = '{"person" : '+personenDaten + ', "contact" : ' + notfallkontakt + ', "location" : '+location+ '}';
 
     return alldata;
 }
 
-function sendData(roomName, occupants, isPrimary) {
+function sendData(roomName, occupants, isPrimary){
     //var personenDaten = android.getPersonenDaten();
     //var kontaktdaten = android.getNotfallKontakt();
 
     //var location = '{"lat":"48.77930", "lng":"9.10717"}';
 
-    var data = mergeAllData(android.getPersonenDaten(), android.getNotfallKontakt(), android.getMyPosition());
-    for (var easyrtcid in occupants) {
-        var button = document.getElementById("button");
-
-        button.onclick = function (easyrtcid) {
-            return function () {
-                easyrtc.sendDataWS(easyrtcid, "message", data);
-            };
-        }
+    var data = mergeAllData(android.getPersonenDaten(), android.getNotfallKontakt(),  android.getMyPosition());
+    for (var easyrtcid in occupants){
+        easyrtc.sendDataWS(easyrtcid, "message", data );
     }
 }
-document.addEventListener('DOMContentLoaded', connect, false);
